9000517389d38a502fd2106956b793e06dc837a8
Joyce Lai <lai.joyce@gmail.com>
2017-09-04 16:52:45 -0700

    modified Server file
    added nodemailer code

--- server.js
+++ server.js
@@ -10,3 +10,36 @@ app.listen(3000, function() {
 	console.log("App listening on Port 3000");
 });
 
+
+const transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'windracer14@gmail.com',
+        pass: '',
+    },
+});
+
+
+app.post("/email/send", function(req, res) {
+	var email = req.body.email;
+	var name = req.body.name;
+	var message = req.body.message;
+
+	const mailOptions = {
+        from: email,
+        to: 'windracer14@gmail.com',
+        subject: 'pooh bear',
+        html: message,
+    };
+
+    transport.sendMail(mailOptions, (error) => {
+        if (error) {
+            console.log(error);
+        }
+    }).then(function() {
+    	res.redirect("/index.html");
+    });
+
+	
+
+});
